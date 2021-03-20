a317240004335bac16d97de02b61c4fa317727e7
Mikael Keri <info@prowling.nu>
2021-03-20 14:36:09 +0100

    first commit
    Gazing at the cloud

---
+++ README.md
@@ -0,0 +1,34 @@
+
+## About:
+
+This script allows you to collect external IPs and URL's (webapp) from any Azure subscriptions that you have access to.
+
+This information is saved to files, so you can use the data in other tools if you fancy.
+But It also allows you to scan the resources using the data collected
+
+## Install:
+
+Install azure cli (az)
+pip3 install -r requirments.txt
+
+## Usage:
+
+azure_cloudgazer.py [-h] [-ip] [-url] [-scan]
+
+## Examples:
+
+# Collects all external IPs
+
+python3 azure_cloudgazer.py -ip
+
+# Crawls all collected URL and retrives title and error status codes
+
+python3 azure_cloudgazer.py -url -scan
+
+
+## FAQ:
+
+Q: Why did you not use the python azure-cli module? Instead of wrapping az?!
+
+A: Next question!
+
---
+++ azure_cloudgazer.py
@@ -0,0 +1,207 @@
+import argparse
+import subprocess
+import json
+import time
+import os
+from datetime import datetime, timedelta
+import nmap3
+from nested_lookup import nested_lookup
+import urllib.request
+from urllib.error import HTTPError
+import urllib.response
+from bs4 import BeautifulSoup
+
+parser = argparse.ArgumentParser(description='Collect external information and scan')
+parser.add_argument('-ip',action='store_true', help='Collect external IP addresses')
+parser.add_argument('-url',action='store_true', help='Collect external URLs')
+parser.add_argument('-scan',action='store_true', help='Scan, just like the name implies (will need -ip or -url')
+args = parser.parse_args()
+
+print("""\                                                                                                                                          
+                                          #                                     
+                                       ###                                      
+                                    #####    ###                                
+                                 #######    (####                               
+                              ########/    /######(                             
+                           ##########     .#########                            
+                        (###########      ###########*                          
+                      #############      ##############                         
+                     #############      ################                        
+                   ##############      ###################                      
+                  #############(      #####################                     
+                /#############.       #######################                   
+               ##############           ######################                  
+             .##############              #####################/                
+            ###############                 #####################               
+           ###############                    ####################              
+         ###############(                       ####################            
+        ###############,                          (##################           
+      (###############                              /##################         
+                                                     .##################        
+                                       ,(################################(      
+                         ,(################################################    
+                         Azure cloudgazer - @nsmfoo  (Mikael Keri)       
+""")
+
+# List subscriptions
+def azure_account ():  
+    d = []
+    subscriptions = json.loads(subprocess.check_output('az account list', shell=True).decode('utf-8'))
+    for i in subscriptions:
+        d.append(i['id'])   
+    return d
+
+def azure_ip (ip_file):
+    f = open(ip_file, "w")
+    ids = azure_account()
+    amount = len(ids)
+    number = 1
+    ip_ext = []
+    for id in ids:
+        print("Searching: " + str(number) + "/" + str(amount) +  " ID: " + id) 
+        set_sub = subprocess.getoutput("az account set --subscription " + id)
+        ext_ips = json.loads(subprocess.getoutput("az graph query -q \"Resources | where type contains 'publicIPAddresses' and isnotempty(properties.ipAddress)| project properties.ipAddress\" -o json"))
+        for pi in ext_ips:
+            ip_ext.append(pi['properties_ipAddress'])
+        number += 1  
+        time.sleep(1)  
+   
+    # Sort output and remove dups
+    list_set = set(ip_ext)
+    unique_list = (list(list_set))
+    f.write('\n'.join(unique_list))
+    print("All done! " +  ip_file + " written to disk")
+    f.close()
+
+def azure_url(url_file):
+    f = open(url_file, "w")
+    ids = azure_account()
+    amount = len(ids)
+    number = 1
+    url_ext = []
+    for id in ids:
+        try: 
+            print("Searching: " + str(number) + "/" + str(amount) +  " ID: " + id) 
+            number += 1  
+            set_sub = subprocess.getoutput("az account set --subscription " + id)
+            ext_url = json.loads(subprocess.getoutput("az webapp list --query \"[].{hostName: defaultHostName, state: state}\" -o json"))
+            for pi in ext_url:
+                url_ext.append(pi['hostName'])
+            time.sleep(2)  
+        except Exception:
+            continue
+
+    for var in url_ext:
+        print(var)  
+    
+     # Sort output and remove dups
+    list_set = set(url_ext)
+    unique_list = (list(list_set))
+    f.write('\n'.join(unique_list))
+    print("All done! " +  url_file + " written to disk")
+    f.close()    
+
+def azure_nmap():
+    o = open(ip_scan_file, "w")
+    azure_ip_in = open(ip_file, 'r').read().split('\n')
+    azure_ip_in_nice = list(filter(None, azure_ip_in))
+    nmap = nmap3.Nmap()
+    count = 1
+    for ips in azure_ip_in_nice:
+        try: 
+            print("* Scanning: " + str(count) + "/" + str(len(azure_ip_in_nice)))
+            count += 1
+            scan_ip = nmap.scan_top_ports(ips, args="-sV -Pn -T2 --open")
+            ports = nested_lookup('portid',scan_ip)
+            prod = nested_lookup('product',scan_ip)    
+            x = 0
+            while x < len(ports):
+                ip_scan_ext = "IP: " + ips + " Port: " + str(ports[x]) + " Possible Service: " + str(prod[x])
+                x += 1
+                print(ip_scan_ext)
+                o.write(ip_scan_ext + "\n")
+        except Exception:
+            continue
+    o.close()
+
+def azure_crawling():
+    o = open(url_scan_file, "w")
+    azure_url_in = open(url_file, 'r').read().split('\n')
+    azure_url_in_nice = list(filter(None, azure_url_in))
+    count = 1
+    req_type = ['http://','https://']
+    for url in azure_url_in_nice:
+       print("* Scanning: " + str(count) + "/" + str(len(azure_url_in_nice))) 
+       count += 1 
+       for req in req_type: 
+            try: 
+                x = urllib.request.urlopen(req + url, timeout = 10)
+                xx = x.read()
+                soup = BeautifulSoup(xx, 'html.parser') 
+                time.sleep(2)  
+                print("URL: " + url + " Status: OK Title: " + soup.title.get_text()) 
+                o.write("URL: " + url + " Status: OK  Title: " + soup.title.get_text() + "\n")
+            except urllib.error.HTTPError as e:
+                print("URL: " + url + " Error: " + str(e)) 
+                o.write("URL: " + url + " Error: " + str(e) + "\n")  
+            except Exception as e:
+               print("URL: " + url + " Error: " + str(e))  
+               o.write("URL: " + url + " Error: " + str(e) + "\n")
+               continue
+    o.close()
+
+# Misc settings
+# IP log file
+ip_file = "azure_ip.lst"
+ip_scan_file = "azure_ip_scan_result.lst"
+# URL log file
+url_file = "azure_url.lst"
+url_scan_file = "azure_url_scan_result.lst"
+
+# Logged in 
+logged_in = subprocess.getoutput("az account list-locations")
+if 'Please run' in logged_in:
+    print("*** Not logged in, please run 'az login' ***")
+    exit() 
+else:
+    print("* Logged in, ready to start!")
+
+if args.ip:
+    # Check if file exist
+    if os.path.isfile(ip_file):
+    # If it exist, check age of the file. No need to burn magic cloud money ..
+        age = datetime.now() - timedelta(days=7)
+        filetime = datetime.fromtimestamp(os.path.getctime(ip_file))
+        if filetime < age:
+            print("* IP file needs to be update ..") 
+            azure_ip(ip_file)         
+        else:
+            print("* IP file is fresh, will not update (and you can't make me)") 
+    else:
+        print("* First run! Populating IP file")
+        azure_ip(ip_file)
+
+if args.url:
+    # Check if file exist
+    if os.path.isfile(url_file):
+    # If it exist, check age of the file. No need to burn magic cloud money ..
+        age = datetime.now() - timedelta(days=7)
+        filetime = datetime.fromtimestamp(os.path.getctime(url_file))
+        if filetime < age:
+            print("* URL file needs to be update ..") 
+            azure_url(url_file)         
+        else:
+            print("* URL file is fresh, will not update (and you can't make me)") 
+    else:
+        print("* First run! Populating URL file")
+        azure_url(url_file)
+
+if args.scan:
+    # Scanning
+    if args.ip:
+        print("* Let's go NMAP scanning! We will go soft, so go and grab some coffee")
+        azure_nmap()
+    # Crawling
+    if args.url:
+         print("* Let's go URL Crawling! We will go soft, so go and grab some coffee")
+         azure_crawling()
